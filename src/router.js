55267a67152085238d27da3e85cfeeef44940970
Jan Blažej <31972521+JBlazej@users.noreply.github.com>
2018-11-28 11:22:47 +0100

    file router

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import {Router} from 'express'
-//import requireDir from 'require-dir'
 import path from 'path'
-import { compileFunction } from 'vm';
 
 const htmlPath = path.join(__dirname, '../public/front/', 'index.html')
 const css = path.join(__dirname, '../public/front/', 'app.css')
@@ -164,5 +162,44 @@ router.get('/img8', (req, res) => {
 router.get('/img9', (req, res) => {
     res.status(200).sendFile(img9)
 })
+
+router.get('/img/:name', tattooFindImage(req, res))
+
+async function tattooFindImage (req, res){
+    try{
+        const options = await getFileOptions()
+        const fileName = req.params.name
+
+        res.sendFile(fileName, options, (err) => {
+            if (err)
+            return res.status(404).json({
+              error: {
+                msg: 'File not found'
+              }
+            })
+        
+            res.status(200)
+        })
+    }catch(error){
+        res.status(400).json({error : 'Bad request'})
+    }
+}
+
+
+const serverImagesPath = path.join(__dirname, '../public/assets/images', 'front')
+
+async function getFileOptions(){
+    const options = {
+        root: serverImagesPath,
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    }
+
+    return options
+}
+
 
 export default router
